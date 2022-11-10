768198ef7525b92d216e597a8c0e18d74777015c
Alona-Liubchenko <aylin888@rambler.ru>
2022-11-10 16:09:56 +0200

    h14

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,47 @@
+// import Layout from './Layout/Layout';
+// import AppBar from './AppBar/AppBar';
+// import TaskForm from './TaskForm/TaskForm';
+// import TaskList from './TaskList/TaskList';
+// export const App = () => {
+//   return (
+//     <Layout>
+//       <AppBar />
+//       <TaskForm />
+//       <TaskList />
+//     </Layout>
+//   );
+// };
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from 'redux/operations';
+import { getError, getIsLoading, getTasks } from 'redux/selectors';
 import Layout from './Layout/Layout';
 import AppBar from './AppBar/AppBar';
 import TaskForm from './TaskForm/TaskForm';
 import TaskList from './TaskList/TaskList';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  // const { items, isLoading, error } = useSelector(getTasks);
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
   return (
+    // <div>
+    //   {isLoading && <p>Loading tasks...</p>}
+    //   {error && <p>{error}</p>}
+    //   <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
+    // </div>
+
     <Layout>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
     </Layout>
+    //
   );
 };
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,13 @@
 import { useDispatch } from 'react-redux';
-import { deleteTask, toggleCompleted } from 'redux/tasksSlice';
+// import {
+//   // deleteTask,
+//   toggleCompleted,
+// } from 'redux/tasksSlice';
+import { deleteTask, toggleCompleted } from 'redux/operations';
 const Task = ({ task }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteTask(task.id));
-  const handleToggle = () => dispatch(toggleCompleted(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
   return (
     <div>
       <input type="checkbox" onChange={handleToggle} checked={task.completed} />
