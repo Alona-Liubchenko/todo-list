77a1c647c70faa04d293c5021d80c2e914dc37fb
Alona-Liubchenko <98355708+Alona-Liubchenko@users.noreply.github.com>
2023-02-04 15:27:55 +0000

    Deploying to gh-pages from @ Alona-Liubchenko/todo-list@925afd84c4dad83327ecf48417c8e16b67b25e8e 🚀

---
+++ static/js/266.8a44ac3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(184);function a(){return(0,r.jsx)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"85vh"},children:(0,r.jsx)("h1",{style:{textAlign:"center"},children:"Task manager welcome page"})})}}}]);
+//# sourceMappingURL=266.8a44ac3b.chunk.js.map
